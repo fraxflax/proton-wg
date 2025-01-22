0f4566614601c2d2d0f77ef363f2abbea2f111bd
fraxflax <frax@axnet.nu>
2025-01-22 15:22:40 +0100

    info

--- proton-wg.sh
+++ proton-wg.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
-#
+# 
 # proton-wg.sh - ProtonVPN Linux Client WireGuard interface setup script
 #
-# brings down/up an wireguard interface named wgproton
-# and sets up the routing and dns via it 
+# brings down/up an ProtonVPN WireGuard interface
+# and sets up the routing and dns via it
 # 
 # proton-wg.sh must be run as root
-#
+# 
 # proton-wg.sh is free software written by Fredrik Ax <proton-wg@axnet.nu>
+# 
 # Feel free to modify and/or (re)distribute it in any way you like.
 # (It's always nice to be mentioned though ;-) )
-#
+# 
 # proton-wg.sh comes with ABSOLUTELY NO WARRANTY.
-#
+# 
 # If you expirence any problems with proton-wg.sh, are lacking any
 # functionality or just want to voice your opions about it, feel free
 # to contact me via e-mail: Fredrik Ax <proton-wg@axnet.nu>
-# (also, if you need a script for setting up a Linux router to route 
+# (also, if you need a script for setting up a Linux router to route
 #  certain subnets via ProtonVPN, feel free to contact me)
 #
-
 protonwg=${0##*/}
 usage() {
     ERRMSG=''; [ "$1" ] && ERRMSG="
